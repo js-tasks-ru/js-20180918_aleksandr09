24789a9a10a7fa202b5c2548004bb470ceab8c89
aleksandrkl <aleksandr.klyashitsky@tutanota.com>
2018-10-01 18:28:10 +0200

    Решил второй модул, вопросы 1 и 2

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -5,4 +5,30 @@
  * @returns {Object}
  */
 function find (obj, value) {
+  let result = [];
+
+  for (let property in obj) {
+    if (obj[property] === value) {
+      result.push (property);
+
+    } else if (typeof obj[property] === "object" && obj[property] != null) {
+      let iResult = find(obj[property], value);
+
+        if (typeof iResult === "string") {
+          result.push(property + '.' + iResult);
+
+        } else if (iResult) {
+          for (let i = 0; i<iResult.length; i++) {
+            result.push(property + '.' + iResult[i]);
+          }
+        }
+    }
+  }
+
+  if (result.length === 0) {
+    return null;
+  } else if (result.length === 1) {
+    return result[0];
+  } else return result;
+
 }
